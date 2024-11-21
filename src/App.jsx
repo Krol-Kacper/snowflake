3e2bdb4c5d7b79e7fdb45d39f65de4df2373c37f
Łukasz Grzegocki <279428@student.pwr.edu.pl>
2024-11-21 20:20:45 +0100

    Day1-snow-header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import Snow from './Snow.jsx'
+import Header from './Header.jsx'
 
 function App() {
 
   return (
     <>
+      <Header></Header>
       <Snow></Snow>
+      <img src="/src/images/background.png" alt="Backgroudn photo" class="bgphoto"></img>
     </>
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+
+    return (
+        <header>
+            <img src="./src/images/snowflake.png" width="90px" height="auto" class="logo" alt="Snowflake image"></img>
+            <h1>Snowflake Casino</h1>
+        </header>
+    )
+}
+
+export default Header
