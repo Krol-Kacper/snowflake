cd5a537c5ffb7cb256a6f82c3f506025da9029b1
Łukasz Grzegocki <279428@student.pwr.edu.pl>
2024-11-22 22:12:35 +0100

    Day2-header-updatev2

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,6 +6,8 @@ function Header() {
         <header>
             <img src="./src/images/snowflake.png" width="80px" height="90px" class="logo" alt="Snowflake image"></img>
             <h1>Snowflake Casino</h1>
+            <button class="wallet-button">Connect Wallet</button>
+            <a class="balance-display">Balance: 0</a>
         </header>
     )
 }
