b6b23c0bf5545c7958e5ee05f561bfeb699ea0e3
Łukasz Grzegocki <279428@student.pwr.edu.pl>
2024-11-23 15:39:51 +0100

    Prize-win-update-byluk

--- src/Infobar.jsx
+++ src/Infobar.jsx
@@ -4,32 +4,32 @@ function Infobar() {
     return (<div class="infobar">
         <h1 class="infobar-title">Prize table</h1>
         <img src="./src/images/ethcoin.png" width="80px" height="80px" alt="ethereum coin image" class="ethcoin"></img>
-        <ul class="prize-table">
-            <li>Snowy Win</li>
-            <img src="./src/images/icecube.png" width="20px" height="20px" alt="icecube image"></img>
-            <img src="./src/images/icecube.png" width="20px" height="20px" alt="icecube image"></img>
-            <img src="./src/images/icecube.png" width="20px" height="20px" alt="icecube image"></img>
+        <div class="prize-table">
+            <h2 class="small-win">Snowy Win</h2>
+            <img src="./src/images/snowman.png" width="20px" height="20px" alt="icecube image"></img>
+            <img src="./src/images/snowman.png" width="20px" height="20px" alt="icecube image"></img>
+            <img src="./src/images/snowman.png" width="20px" height="20px" alt="icecube image"></img>
             <a> = </a>
             <a class="multiplier">x1.5</a>
-            <li>Cool Win</li>
+            <h2 class="small-win">Cool Win</h2>
             <img src="./src/images/icecube.png" width="20px" height="20px" alt="icecube image"></img>
             <img src="./src/images/icecube.png" width="20px" height="20px" alt="icecube image"></img>
             <img src="./src/images/icecube.png" width="20px" height="20px" alt="icecube image"></img>
             <a> = </a>
             <a class="multiplier">x2</a>
-            <li>Hot Win</li>
-            <img src="./src/images/icecube.png" width="20px" height="20px" alt="icecube image"></img>
-            <img src="./src/images/icecube.png" width="20px" height="20px" alt="icecube image"></img>
-            <img src="./src/images/icecube.png" width="20px" height="20px" alt="icecube image"></img>
+            <h2 class="hot-win">Hot Win</h2>
+            <img src="./src/images/kakao.png" width="20px" height="20px" alt="hot chocolate image"></img>
+            <img src="./src/images/kakao.png" width="20px" height="20px" alt="hot chocolate icecube image"></img>
+            <img src="./src/images/kakao.png" width="20px" height="20px" alt="hot chocolate icecube image"></img>
             <a> = </a>
             <a class="multiplier">x5</a>
-            <li>Snowflake Mega Big Win</li>
+            <h2 class="big-win">Snowflake Mega Big Win</h2>
             <img src="./src/images/snowflake.png" width="20px" height="20px" alt="icecube image"></img>
             <img src="./src/images/snowflake.png" width="20px" height="20px" alt="icecube image"></img>
             <img src="./src/images/snowflake.png" width="20px" height="20px" alt="icecube image"></img>
             <a> = </a>
             <a class="multiplier">x20</a>
-        </ul>
+        </div>
     </div>)
 }
 
