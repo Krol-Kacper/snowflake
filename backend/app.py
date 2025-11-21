4ee87367991595747f4bd0c9d82cd4a8bade993c
Krol-Kacper <279415@student.pwr.edu.pl>
2025-11-21 22:45:04 +0100

    backend for web3 (not tested)

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,8 @@ from dotenv import load_dotenv
 
 from datetime import datetime
 
+
+
 app = Flask(__name__)
 load_dotenv()
 try:
@@ -29,7 +31,7 @@ app.config['MONGO_URI'] = "mongodb://mongodb-service.default:27017/database" #"m
 try:
     time.sleep(2)
     mongo = PyMongo(app)
-    mongo.db.users.find_one({}) 
+    mongo.db.users.find_one({})
     print("Connected to MongoDB successfully.")
 except PyMongoError as e:
     print(f"Failed to connect to MongoDB: {e}")
@@ -85,10 +87,10 @@ def spin():
 
     data = request.get_json() or {}
     token = data.get('token')
-    
+
     try:
         payload = jwt.decode(token, jwt_secret, algorithms=jwt_algorithm)
-    except:
+    except Exception:
         return jsonify({'message': 'Expired Token'}), 402
     #print(payload)
 
@@ -100,7 +102,7 @@ def spin():
 
     if bet > balance:
         return jsonify({'message':'Inssuficient balance'}), 400
-    
+
     #------------------------------------------ Losowanie
 
     alphabet = string.digits
@@ -116,7 +118,7 @@ def spin():
 
     if result_int <= 999 and result_int >= 980: # 2% na wina x20
         result = "333"
-    
+
     elif result_int <= 691 and result_int >= 641: # 5% na wina x5
         result = "222"
 
@@ -145,7 +147,7 @@ def spin():
 
         for i in list:
             result += str(i)
-        
+
 
     #print(result_int)
     #print(result)
